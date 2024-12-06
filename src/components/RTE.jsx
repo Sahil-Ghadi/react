007288350f2be08b9ba210c05ed1aba555714dfb
pradnya587 <pradnyaghadi885@gmail.com>
2024-12-06 08:29:48 +0530

    tinymce apikey

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
+import conf from '../conf/conf.js'
 
 export default function RTE({name, control, label, defaultValue = ""}) {
+  const apiKey = conf.tinymceApiKey;
   return (
     <div className='w-full'>
       {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -12,7 +14,7 @@ export default function RTE({name, control, label, defaultValue = ""}) {
       control={control}
       render={({field: {onChange}})=>(
         <Editor
-        apiKey='bfhl99wyuhzpfmcvhw8qgwz60rtxbmpa5g4toeltac5dhbc2'
+        apiKey
         initialValue:defaultValue
         init={{
           initialValue: defaultValue,
