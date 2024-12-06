480dfb60aa80186c113c82867355798f46f0b300
pradnya587 <pradnyaghadi885@gmail.com>
2024-12-06 19:55:08 +0530

    new

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue = ""}) {
       control={control}
       render={({field: {onChange}})=>(
         <Editor
-        apiKey={conf.tinymceApiKey}
+        apiKey={conf.tinymceApiID}
         initialValue={defaultValue}
         init={{
           initialValue: defaultValue,
